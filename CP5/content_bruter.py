ebddc5a05567a2c25925b6f83fba3da33f5dc1dd
agodoriru <takumijohn0806@gmail.com>
2017-12-01 15:01:24 +0000

    hoge

--- CP5/content_bruter.py
+++ CP5/content_bruter.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
 import urllib2
@@ -6,33 +7,77 @@ import threading
 import Queue
 
 threads=50
-target_url="http://testphp/vulnweb.com"
-wordlist_file="tmp/all.txt"
+target_url="http://testphp.vulnweb.com"
+wordlist_file="all.txt"
 resume=None
+user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
 
-#user_agent="Mozilla"
+
 
 def build_wordlist(wordlist_file):
     fd=open(wordlist_file,"rb")
-    raw_words=fd.readline()
+    raw_words=fd.readlines()
     fd.close()
 
-found_resume=False
-words=Queue.Queue()
+    found_resume=False
+    words=Queue.Queue()
 
-for word in raw_words:
-    word=word.rstrip()
+    for word in raw_words:
+        word=word.rstrip()
     
-    if resume is not None:
-        if found_resume:
-            words.put(word)
+        if resume is not None:
+            
+            if found_resume:
+                words.put(word)
+            else:
+                if word == resume:
+                    found_resume=True
+                    print "Resuming wordlist from %s" % resume
+        
         else:
-            found_resume=True
-            print "Resuming wordlist from %s"%resume
-    else:
-        words.put(word)
+            words.put(word)
 
-return words
-    
+    return words
 
+def dir_bruter(word_queue,extensions=None):
+    while not word_queue.empty():
+        attempt=word_queue.get()
+        
+        attempt_list=[]
+        
+        if "." not in attempt:
+            attempt_list.append("/%s/" % attempt)
+        else:
+            attempt_list.append("/%s" % attempt)
+            
+        if extensions:
+            for extension in extensions:
+                attempt_list.append("/%s%s"% (attempt,extension))
+        
+        for brute in attempt_list:
+            url="%s%s" % (target_url,urllib.quote(brute))
+            
+            try:
+                headers={}
+                headers["User-agent"]=user_agent
+                
+                r=urllib2.Request(url,headers=headers)
+                
+                response=urllib2.urlopen(r)
+                
+                if len(response.read()):
+                    print " [%d] -> %s" % (response.code,url)
+                    
+            except urllib2.URLError,e:
+                
+                if hasattr(e,'code') and e.code != 404:
+                    print "!!! %d -> %s" %(e.code,url)
+                    
+                pass
 
+word_queue=build_wordlist(wordlist_file)
+extensions=[".php",".bak",".orig",".inc"]
+            
+for i in range(threads):
+    t=threading.Thread(target=dir_bruter,args=(word_queue,extensions,))
+    t.start()
